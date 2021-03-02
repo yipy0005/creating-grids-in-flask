25277e3ad07cf8ed446220a7b44817b89c9f3df2
Yew Mun <yipy0005@gmail.com>
2021-03-03 00:01:16 +0800

    Updated on 2 Mar 2021

--- 2wdproj_py37/app/grid_map/views.py
+++ 2wdproj_py37/app/grid_map/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request
 
 from app.grid_map.forms import OverallRackParams, IndividualRackParams
 
@@ -12,39 +12,66 @@ gridmap = Blueprint(
 
 @gridmap.route('/overall', methods=['GET', 'POST'])
 def overall_racking():
-    all_alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    # all_alphabets = [
+    #     'A', 'B', 'C', 'D', 'E', 'F', 'G',
+    #     'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    #     'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    #     'V', 'W', 'X', 'Y', 'Z'
+    # ]
 
     form = OverallRackParams(request.form)
 
-    if request.method == 'POST' and form.validate(): #or obstacle_form.validate():
+    if request.method == 'POST' and form.validate():
+        no_of_rows = int(
+            form.total_no_of_racks.data / form.no_of_racks_per_row.data
+        )
+        no_of_cols = int(
+            form.total_no_of_racks.data / form.no_of_racks_per_col.data
+        )
 
-        no_of_rows = int(form.total_no_of_racks.data / form.no_of_racks_per_row.data)
-        no_of_cols = int(form.total_no_of_racks.data / form.no_of_racks_per_col.data)
+        all_rack_coords = []
+        for y in range(no_of_cols):
+            for x in range(no_of_rows):
+                all_rack_coords.append((x, y))
 
         # all_alphabets = all_alphabets[0:no_of_cols]
 
         # all_rack_coords = []
-        all_racks = []
-        
+        # all_racks = []
+
         rack_data = {}
         rack_data['rack_info'] = []
         # for alphabet in all_alphabets:
         for i in range(no_of_rows*no_of_cols):
             # all_rack_coords.append(alphabet+str(i))
-            all_racks.append('rack'+str(i))
+            # all_racks.append('rack'+str(i))
             rack_data['rack_info'].append(
                 {
                     'rack_id': 'rack'+str(i), #alphabet+str(i),
+                    'rack_coords': all_rack_coords[i],
                     'height': 0,
                     'width': 0
                 }
             )
-        
+
         with open('racks_infos.txt', 'w') as outfile:
             json.dump(rack_data, outfile, indent=4)
 
-        return render_template('all_rack_coords.html', all_racks=all_racks)
-                    
+        grid_map(
+            no_of_rows,
+            no_of_cols,
+            no_of_rows*no_of_cols,
+            0
+        )
+
+        return render_template(
+            'all_rack_coords.html',
+            # all_racks=all_racks,
+            rack_no=rack_data['rack_info'],
+            no_of_rows=no_of_rows,
+            no_of_cols=no_of_cols
+        )
+
     return render_template('overall_rack_form.html', form=form)
 
 
@@ -65,12 +92,16 @@ def individual_racking(rackid):
             if request.method == 'POST' and form.validate():
                 each_rack['height'] = int(form.rack_height.data)
                 each_rack['width'] = int(form.rack_width.data)
-    
+
     with open('racks_infos.txt', 'w') as json_file:
         json.dump(rack, json_file, indent=4)
-    
-        return render_template('individual_rack_form.html', form=form, rack_id=rack_id)
-    
+
+        return render_template(
+            'individual_rack_form.html',
+            form=form,
+            rack_id=rack_id
+        )
+
     return render_template('all_rack_coords.html', all_racks=all_racks)
 
 
@@ -80,6 +111,53 @@ def mapping():
     with open('racks_infos.txt', 'r') as json_file:
         rack = json.load(json_file)
 
-    grid_map()
+    total_required_rows = []
+    total_racks_in_a_row = []
+    total_required_cols = []
+    total_racks_in_a_col = []
 
-    return render_template('map.html')
+    for each_rack in rack['rack_info']:
+        total_racks_in_a_row.append(each_rack['rack_coords'][0])
+        total_racks_in_a_col.append(each_rack['rack_coords'][1])
+
+    total_racks_in_a_row = len(set(total_racks_in_a_row))
+    total_racks_in_a_col = len(set(total_racks_in_a_col))
+
+    for x in range(total_racks_in_a_row):
+        required_rows = []
+        for each_rack in rack['rack_info']:
+            if each_rack['rack_coords'][0] == x:
+                required_rows.append(each_rack['height'])
+        total_required_rows.append(max(required_rows))
+    
+    for y in range(total_racks_in_a_col):
+        required_cols = []
+        for each_rack in rack['rack_info']:
+            if each_rack['rack_coords'][1] == y:
+                required_cols.append(each_rack['width'])
+        total_required_cols.append(max(required_cols))
+
+    total_required_rows = sum(total_required_rows) + total_racks_in_a_row + 1
+    total_required_cols = sum(total_required_cols) + total_racks_in_a_col + 1
+
+    all_possible_coordinates = []
+    for x in range(total_required_rows):
+        for y in range(total_required_cols):
+            all_possible_coordinates.append([x, y])
+
+    racks_coords = []
+    for each_rack in rack['rack_info']:
+        each_rack_coords = [each_rack['width'], each_rack['height']]
+
+    grid_map(
+        total_required_rows,
+        total_required_cols,
+        total_racks_in_a_row*total_racks_in_a_col,
+        1
+    )
+
+    return render_template(
+        'map.html',
+        all_possible_coordinates=all_possible_coordinates,
+        racks_coords=racks_coords
+    )
--- 2wdproj_py37/app/third_party_apps/grid_up.py
+++ 2wdproj_py37/app/third_party_apps/grid_up.py
@@ -1,89 +1,174 @@
-def grid_map(length, width):
-    with open("app/templates/map.html", "w") as map_file:
+def grid_map(rows, cols, total_racks, rack_specific_details=0):
 
-        all_alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-        all_numbers = [i for i in range(26)]
+    if rack_specific_details == 0:
 
-        alphabets = []
-        numbers = []
-        for i in range(length):
-            numbers.append(all_numbers[i])
-        for i in range(width):
-            alphabets.append(all_alphabets[i])
-        
-        coordinate_system = []
-        for alphabet in alphabets:
-            for number in numbers:
-                coordinate_system.append(alphabet + str(number))
-        
-        print(coordinate_system)
-            
+        with open("app/templates/all_rack_coords.html", "w") as map_file:
 
-        map_file.write(
-            """
-            <!DOCTYPE html>
-            <html>
-            <head>
-            <title>Map</title>
-            <style>
-            """
-        )
-                
-        map_file.write(
-            """
-            #main {
-                height: %ipx;
-                width: %ipx;
-                line-height: 0;
-            }
-            """ % (50*length, 50*width)
-        )
+            # all_alphabets = [
+            #     'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            #     'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            #     'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            #     'V', 'W', 'X', 'Y', 'Z'
+            # ]
+            # all_numbers = [i for i in range(26)]
 
-        map_file.write(
-            """
-            </style>
-            </head>
-            <body>
+            # alphabets = []
+            # numbers = []
+            # for i in range(length):
+            #     numbers.append(all_numbers[i])
+            # for i in range(width):
+            #     alphabets.append(all_alphabets[i])
 
-            <h1>Grid Map</h1>
-            <br><br>
-            <div id="main">
-            """
-        )
+            # coordinate_system = []
+            # for alphabet in alphabets:
+            #     for number in numbers:
+            #         coordinate_system.append(alphabet + str(number))
 
-        # 1D Matrix
-        for coordinates in coordinate_system:
+            # print(coordinate_system)
+
             map_file.write(
                 """
-                <img class="myButton" id="%s" src="{{ url_for('static', filename='image1.png') }}" style="width:50px;height:50px;float:left;">
-                """ % coordinates
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <title>General Map</title>
+                """
             )
-        
-        map_file.write(
-            """
-            <script>
-                var myButton = document.getElementsByClassName("myButton");
-                var i;
-                for (i = 0; i < myButton.length; i++) {
-                    var imgList = ["{{ url_for('static', filename='image1.png') }}","{{ url_for('static', filename='image2.png') }}", "{{ url_for('static', filename='image3.png') }}"];
 
-                    var ButtonID = document.getElementsByClassName('myButton')[i].id
-                    var myImage = document.getElementById(ButtonID)
-                    myImage.onclick = function( e ){
-                        var elem = e.target,
-                        imageIndex = parseInt(elem.dataset.img,10);
-                        if( imageIndex <= (imgList.length -1) ) {
-                            elem.src = imgList[imageIndex++];
-                            elem.dataset.img = imageIndex;
-                        } else {
-                            elem.src = imgList[0];
-                            elem.dataset.img = 1;
-                        }
-                    }
+            # map_file.write(
+            #     """
+            #     <style>
+            #     #overall_div {
+            #         height: %ipx;
+            #         width: %ipx;
+            #         line-height: 0;
+            #     }
+            #     </style>
+            #     """ % (50*cols, 50*rows)
+            # )
+
+            map_file.write(
+                """
+                </head>
+                <body>
+
+                <h1>General Map</h1>
+                <br><br>
+                <table>
+                """
+            )
+
+            map_file.write(
+                "{% for x in range("+str(rows)+") %}"
+                "<tr>"
+                "    {% for y in range("+str(cols)+") %}"
+                "        {% for i in range(rack_no|length) %}"
+                "            {% if rack_no[i]['rack_coords'][0] == x and rack_no[i]['rack_coords'][1] == y %}"
+                """
+                                <td>
+                                    <a href="/grid/individual/{{ rack_no[i]['rack_id'] }}" target="_blank">{{ rack_no[i]['rack_id'] }}</a>
+                                </td>
+                            {% endif %}
+                        {% endfor %}
+                    {% endfor %}
+                </tr>
+                {% endfor %}
+                """
+            )
+
+    # else:
+    #     with open("app/templates/all_rack_coords.html", "w") as map_file:
+    #         map_file.write('')
+
+    elif rack_specific_details == 1:
+
+        with open("app/templates/map.html", "w") as map_file:
+
+            map_file.write(
+                """
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <title>Actual Map</title>
+                """
+            )
+
+            map_file.write(
+                """
+                <style>
+                #main {
+                    height: %ipx;
+                    width: %ipx;
+                    line-height: 0;
                 }
-            </script>
-            </div>
-            </body>
-            </html>
-            """
-        )
+                </style>
+                """ % (50*rows, 50*cols)
+            )
+
+            map_file.write(
+                """
+                </head>
+                <body>
+
+                <h1>Actual Map</h1>
+                <br><br>
+                <div id="main">
+                """
+            )
+
+            map_file.write(
+                """
+                {% for coord in all_possible_coordinates %}
+                    {% if coord in racks_coords %}
+                        <img src="{{ url_for('static', filename='image2.png') }}" style="width:50px;height:50px;float:left;">
+                    {% else %}
+                        <img src="{{ url_for('static', filename='image1.png') }}" style="width:50px;height:50px;float:left;">
+                    {% endif %}
+                {% endfor %}
+                """
+            )
+
+            map_file.write(
+                """
+                </div>
+                </body>
+                </html>
+                """
+            )
+
+        # # 1D Matrix
+        # for coordinates in coordinate_system:
+        #     map_file.write(
+        #         """
+        #         <img class="myButton" id="%s" src="{# {{ url_for('static', filename='image1.png') }} #}" style="width:50px;height:50px;float:left;">
+        #         """ % coordinates
+        #     )
+
+        # map_file.write(
+        #     """
+        #     <script>
+        #         var myButton = document.getElementsByClassName("myButton");
+        #         var i;
+        #         for (i = 0; i < myButton.length; i++) {
+        #             var imgList = ["{# {{ url_for('static', filename='image1.png') }} #}","{# {{ url_for('static', filename='image2.png') }} #}", "{# {{ url_for('static', filename='image3.png') }} #}"];
+
+        #             var ButtonID = document.getElementsByClassName('myButton')[i].id
+        #             var myImage = document.getElementById(ButtonID)
+        #             myImage.onclick = function( e ){
+        #                 var elem = e.target,
+        #                 imageIndex = parseInt(elem.dataset.img,10);
+        #                 if( imageIndex <= (imgList.length -1) ) {
+        #                     elem.src = imgList[imageIndex++];
+        #                     elem.dataset.img = imageIndex;
+        #                 } else {
+        #                     elem.src = imgList[0];
+        #                     elem.dataset.img = 1;
+        #                 }
+        #             }
+        #         }
+        #     </script>
+        #     </div>
+        #     </body>
+        #     </html>
+        #     """
+        # )
